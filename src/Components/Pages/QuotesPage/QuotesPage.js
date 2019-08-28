5ab1b7279230b97e80dbf92b1881b201a206ede9
Bassem Mohamed <bassemmohamed1994@gmail.com>
2019-08-28 11:36:05 +0200

    Added new Quote

--- src/Components/Pages/QuotesPage/QuotesPage.js
+++ src/Components/Pages/QuotesPage/QuotesPage.js
@@ -6,18 +6,21 @@ const QuotesWrapper = styled.div``;
 
 const QuotesPage = () => (
   <QuotesWrapper>
-    <Quote author="Theodore N. Vail" align="left">
+    <Quote author="Aristotle" align="left">
+      We are what we repeatedly do. Excellence then is not an act but a habit
+    </Quote>
+    <Quote author="Theodore N. Vail" align="right">
       Real difficulties can be overcome. It is only the imaginary ones that are
       unconquerable
     </Quote>
-    <Quote author="James Clear" align="right">
+    <Quote author="James Clear" align="left">
       Happiness is the space between one desire being fulfilled and a new desire
       forming
     </Quote>
-    <Quote author="Friedrich Nietzsche" align="left">
+    <Quote author="Friedrich Nietzsche" align="right">
       He who has a why to live for can bear almost any how.
     </Quote>
-    <Quote author="Tim Cook" align="right">
+    <Quote author="Tim Cook" align="left">
       If the service is ‘free’, you are not the customer but the product
     </Quote>
   </QuotesWrapper>
