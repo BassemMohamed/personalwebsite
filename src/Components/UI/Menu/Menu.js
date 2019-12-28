c998758300597219ffe4d13f5feeef44280218bf
Bassem Mohamed <bassemmohamed1994@gmail.com>
2019-12-28 12:15:05 +0200

    Menu remove scrollbar & autoclose

--- src/Components/UI/Menu/Menu.js
+++ src/Components/UI/Menu/Menu.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { slide as Slide } from "react-burger-menu";
 import MainRoutes from "../../../Routes/MainRoutes";
 
 const Menu = () => {
   let { pathname } = useLocation();
+  let [open, setOpen] = useState(false);
+
+  const onStateChange = state => {
+    setOpen(state.isOpen);
+  };
 
   return (
-    <Slide pageWrapId={"page-wrapper"}>
+    <Slide
+      isOpen={open}
+      onStateChange={onStateChange}
+      pageWrapId={"page-wrapper"}
+    >
       {MainRoutes.map(route => {
         let className = "";
         className += pathname === route.key ? "active " : "";
         className += route.disabled ? "disabled " : "";
         return (
-          <div className={className} key={route.key}>
+          <div
+            key={route.key}
+            className={className}
+            onClick={() => setOpen(false)}
+          >
             <Link to={route.key}>
               {route.icon}
               {route.name}
