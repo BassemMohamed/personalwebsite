20a2c4383bc9188c04469e690f17b7698320ea34
Bassem Mohamed <bassemmohamed1994@gmail.com>
2020-02-20 19:05:08 +0200

    Center contact form in page

--- src/Components/Pages/ContactPage/ContactPage.style.js
+++ src/Components/Pages/ContactPage/ContactPage.style.js
@@ -1,10 +1,17 @@
 import { css } from "styled-components";
 
 export default css`
+  height: calc(100vh - 7rem);
+  position: relative;
+
   form {
+    top: 50%;
+    left: 50%;
     width: 50%;
-    margin: 0 auto;
     min-width: 200px;
+    max-width: 700px;
+    position: absolute;
+    transform: translate(-50%, -50%);
   }
 
   form h2 {
